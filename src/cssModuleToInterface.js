46c50f3c5068a58a9dc55aded29154a2d5019862
John Tran <john.tran@onehopewine.com>
2017-09-07 12:03:00 -0700

    Add default exports for TS files

--- src/cssModuleToInterface.js
+++ src/cssModuleToInterface.js
@@ -1,53 +1,64 @@
 import path from 'path';
 
-const filenameToInterfaceName = (filename) => {
-  return path.basename(filename)
+const filenameToInterfaceName = filename => {
+  return path
+    .basename(filename)
     .replace(/^(\w)/, (_, c) => 'I' + c.toUpperCase())
     .replace(/\W+(\w)/g, (_, c) => c.toUpperCase());
 };
 
-const cssModuleToTypescriptInterfaceProperties = (cssModuleKeys, indent = '  ') => {
-  return cssModuleKeys
-    .map((key) => `${indent}'${key}': string;`)
-    .join('\n');
+const cssModuleToTypescriptInterfaceProperties = (
+  cssModuleKeys,
+  indent = '  ',
+) => {
+  return cssModuleKeys.map(key => `${indent}'${key}': string;`).join('\n');
 };
 
-const cssModuleToNamedExports = (cssModuleKeys) => {
-  return cssModuleKeys
-    .map((key) => `export const ${key}: string;`)
-    .join('\n');
+const cssModuleToNamedExports = cssModuleKeys => {
+  return cssModuleKeys.map(key => `export const ${key}: string;`).join('\n');
 };
 
 const allWordsRegexp = /^\w+$/i;
-export const filterNonWordClasses = (cssModuleKeys) => {
-  const filteredClassNames = cssModuleKeys.filter(classname => allWordsRegexp.test(classname));
+export const filterNonWordClasses = cssModuleKeys => {
+  const filteredClassNames = cssModuleKeys.filter(classname =>
+    allWordsRegexp.test(classname),
+  );
   if (filteredClassNames.length === cssModuleKeys.length) {
-    return [filteredClassNames, [],];
+    return [filteredClassNames, []];
   }
-  const nonWordClassNames = cssModuleKeys.filter(classname => !allWordsRegexp.test(classname));
-  return [filteredClassNames, nonWordClassNames,];
+  const nonWordClassNames = cssModuleKeys.filter(
+    classname => !allWordsRegexp.test(classname),
+  );
+  return [filteredClassNames, nonWordClassNames];
 };
 
-export const filenameToTypingsFilename = (filename) => {
+export const filenameToTypingsFilename = filename => {
   const dirName = path.dirname(filename);
   const baseName = path.basename(filename);
   return path.join(dirName, `${baseName}.d.ts`);
 };
 
-export const generateNamedExports = (cssModuleKeys) => {
+export const generateNamedExports = cssModuleKeys => {
   const namedExports = cssModuleToNamedExports(cssModuleKeys);
-  return (`${namedExports}
-`);
+  return `${namedExports}
+`;
 };
 
-export const generateGenericExportInterface = (cssModuleKeys, filename, indent) => {
+export const generateGenericExportInterface = (
+  cssModuleKeys,
+  filename,
+  indent,
+) => {
   const interfaceName = filenameToInterfaceName(filename);
-  const interfaceProperties = cssModuleToTypescriptInterfaceProperties(cssModuleKeys, indent);
-  return (
-`export interface ${interfaceName} {
+  const interfaceProperties = cssModuleToTypescriptInterfaceProperties(
+    cssModuleKeys,
+    indent,
+  );
+  return `export interface ${interfaceName} {
 ${interfaceProperties}
 }
 
-export const locals: ${interfaceName};
-`);
+const locals: ${interfaceName};
+export default locals;
+`;
 };
